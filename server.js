c0d3fbb1d5940d5fc5ff56eef2c4f11ee7ebc596
janSochaOfficial <janek111111666666@gmail.com>
2023-04-19 14:34:47 +0200

    added couple funtions to server.js new file, folder, delete file, folder

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 const path = require("path")
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const fs = require("fs").promises
+
 
 app.set('views', path.join(__dirname, 'views'));         // ustalamy katalog views
 app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));   // domyślny layout, potem można go zmienić
@@ -19,16 +21,15 @@ const helpers = {
     }
 }
 
-const addons = ["peperoni", "mushroms", "pinaple"]
+const uploadPath = path.join(__dirname, "upload");
 
 const context = {
-    helpers,
-    list: ["milk", "butter", "bread"]
-
+    helpers
 }
 
-
 app.get("/", function (req, res) {
+    newFile("plik")
+    deleteFile("plik.txt")
     res.render('home.hbs', context);
 })
 
@@ -48,3 +49,53 @@ app.post("/post", function (req, res) {
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT, "\nhttp://localhost:" + PORT)
 })
+
+async function newFile(fileName, folderPath = ""){
+    let name = fileName
+    while (true){
+        try {
+            if(await isInFolder(name + ".txt", folderPath)){
+                name += "_kopia"
+                continue;
+            }
+            await fs.writeFile(path.join(uploadPath, folderPath, `${name}.txt`), "aa")
+            break;
+        }
+        catch {
+            console.error("nie dodano");
+            break;
+        }
+    }
+}
+async function newFolder(folderName, folderPath = "") {
+    let name = folderName
+    while (isInFolder(name, folderPath)){
+        name += "_kopia"
+    }
+
+    await fs.mkdir(path.join(uploadPath, folderPath, folderName))
+}
+
+async function isInFolder(fileName, folderPath = "") {
+    return fs.promises.access(path.join(uploadPath, folderPath), fs.constants.F_OK)
+        .then(() => true)
+        .catch(() => false)
+}
+
+async function deleteFolder(folderName, folderPath = ""){
+    try{
+        await fs.rmdir(path.join(uploadPath, folderPath, folderName))
+    }
+    catch {
+        console.error("nie usinięto");
+    }
+}
+
+async function deleteFile(fileName, folderPath = ""){
+    try{
+        await fs.unlink(path.join(uploadPath, folderPath, fileName))
+    }
+    catch {
+        console.error("nie usinięto");
+    }
+}
